bcf9b49df9c3fefd271b020dc08950456fd895cb
Vico Melo <vinicomelo@gmail.com>
2019-09-04 11:58:58 -0400

    made question immutable

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import './question.dart';
 
 void main() => runApp(MyApp());
 
@@ -34,7 +35,7 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(questions[_questionIndex]),
+            Question(questions[_questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
               onPressed: _answerQuestion,
