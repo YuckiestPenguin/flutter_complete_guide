af4835af8bf6f47458bb10af524f65cd43f62c23
Vico Melo <vinicomelo@gmail.com>
2019-09-04 10:18:16 -0400

    spacing

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    void answerQuestion(){
+    void answerQuestion() {
       print('Answer Chosen!');
     }
 
