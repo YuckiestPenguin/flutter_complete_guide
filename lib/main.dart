8956dbba7d6f0e982b93f16ba7863b1d59843828
Vico <vinicomelo@gmail.com>
2019-09-09 11:44:25 -0400

    Added result phrase

--- lib/main.dart
+++ lib/main.dart
@@ -15,24 +15,41 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  var _totalScore = 0;
   final _questions = const [
     {
       'questionText': 'What\'s your favorite color?',
-      'answers': ['Blue', 'Red', 'Black', 'White']
+      'answers': [
+        {'text': 'Blue', 'score': 1},
+        {'text': 'Red', 'score': 7},
+        {'text': 'Black', 'score': 10},
+        {'text': 'White', 'score': 3}
+      ]
     },
     {
       'questionText': 'What\'s your favorite animal?',
-      'answers': ['Dog', 'Elephant', 'Lion', 'Fish']
+      'answers': [
+        {'text': 'Dog', 'score': 1},
+        {'text': 'Elephant', 'score': 3},
+        {'text': 'Lion', 'score': 7},
+        {'text': 'Fish', 'score': 10}
+      ]
     },
     {
       'questionText': 'who\'s your favorite team?',
-      'answers': ['Manchester United', 'PSG', 'Real Madrid', 'Bayern']
+      'answers': [
+        {'text': 'Manchester United', 'score': 1},
+        {'text': 'PSG', 'score': 7},
+        {'text': 'Real Madrid', 'score': 10},
+        {'text': 'Bayern', 'score': 5}
+      ]
     },
   ];
 
   @override
   Widget build(BuildContext context) {
-    void _answerQuestion() {
+    void _answerQuestion(int score) {
+      _totalScore += score;
       setState(() {
         _questionIndex = _questionIndex + 1;
       });
@@ -50,7 +67,7 @@ class _MyAppState extends State<MyApp> {
                   answerQuestion: _answerQuestion,
                   questionIndex: _questionIndex,
                 )
-              : Result()),
+              : Result(_totalScore)),
     );
   }
 }
--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    String resultText;
+
+    if (resultScore <= 8) {
+      resultText = 'You are awesome';
+    } else if (resultScore <= 12) {
+      resultText = 'You\'re OK';
+    } else if (resultScore <= 16) {
+      resultText = 'You\'re a little strange';
+    } else {
+      resultText = 'Go away';
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("You Did It!"),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
